1a0bb24fe8042ee4fd3f31402cd20d86e45a45f9
Bertold Van den Bergh <vandenbergh@bertold.org>
2019-12-16 20:02:27 +0100

    Handle case where config file is not defined

--- src/modules/config.js
+++ src/modules/config.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 configPath = process.env.CONFIG_PATH
 
 if(configPath){
+    config = null
     try {
         config = JSON.parse(fs.readFileSync(configPath))
     }catch(e){
